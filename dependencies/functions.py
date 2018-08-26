21cc23026f5e22e9ed16061b6ca7d4310e00184c
joynt <yurinalesso@gmail.com>
2018-08-27 00:48:24 +0200

    Aggiunto multithreading processing e downsampling

--- dependencies/functions.py
+++ dependencies/functions.py
@@ -16,11 +16,92 @@ from IPython.display import clear_output, Image, display, HTML
 from sklearn.metrics import confusion_matrix
 import itertools
 
-    
+import hashlib
+import re
+import os
+import librosa
+def which_set(filename, validation_percentage, testing_percentage, totClass):
+    """Determines which data partition the file should belong to.
+    We want to keep files in the same training, validation, or testing sets even
+    if new ones are added over time. This makes it less likely that testing
+    samples will accidentally be reused in training when long runs are restarted
+    for example. To keep this stability, a hash of the filename is taken and used
+    to determine which set it should belong to. This determination only depends on
+    the name and the set proportions, so it won't change as other files are added.
+    It's also useful to associate particular files as related (for example words
+    spoken by the same person), so anything after '_nohash_' in a filename is
+    ignored for set determination. This ensures that 'bobby_nohash_0.wav' and
+    'bobby_nohash_1.wav' are always in the same set, for example.
+    Args:
+    filename: File path of the data sample.
+    validation_percentage: How much of the data set to use for validation.
+    testing_percentage: How much of the data set to use for testing.
+    Returns:
+    String, one of 'training', 'validation', or 'testing'.
+    """
+    base_name = os.path.basename(filename)
+    # We want to ignore anything after '_nohash_' in the file name when
+    # deciding which set to put a wav in, so the data set creator has a way of
+    # grouping wavs that are close variations of each other.
+    hash_name = re.sub(r'_nohash_.*$', '', base_name)
+    # This looks a bit magical, but we need to decide whether this file should
+    # go into the training, testing, or validation sets, and we want to keep
+    # existing files in the same set even if more files are subsequently
+    # added.
+    # To do that, we need a stable way of deciding based on just the file name
+    # itself, so we do a hash of that and then use that to generate a
+    # probability value that we use to assign it.
+    hash_name_hashed = hashlib.sha1(hash_name.encode('utf-8')).hexdigest()
+    percentage_hash = ((int(hash_name_hashed, 16) %
+                      (totClass + 1)) *
+                     (100.0 / totClass))
+    if percentage_hash < validation_percentage:
+        result = 'validation'
+    elif percentage_hash < (testing_percentage + validation_percentage):
+        result = 'testing'
+    else:
+        result = 'training'
+    return result
+
+#shift signal of #value samples padding with zero to return the same dimension
+def shiftVec(signal, value):
+    initial_length = signal.shape[0]
+    padded = np.pad(signal, (abs(value),abs(value)), 'constant', constant_values=0)
+    signal = padded[abs(value)-value:abs(value)+initial_length-value]
+    return signal
+#return a random noise of nSample
+def noiseSelector(noise, nSample):
+    length = len(noise)
+    choice = rnd.randint(0, length-1)
+    key = list(noise.keys())[choice]
+    start = rnd.randint(0, noise[key].shape[0]-nSample-1)
+    return noise[key][start:start+nSample]
+
+## Return the word between two string starting from left
+def find_between( s, first, last ):
+    try:
+        start = s.index( first ) + len( first )
+        end = s.index( last, start )
+        return s[start:end]
+    except ValueError:
+        return ""
+
+# stretching the sound
+def stretch(data, rate=1):
+    input_length = data.shape[0]
+    data = librosa.effects.time_stretch(data, rate)
+    if len(data)>input_length:
+        data = data[round((data.shape[0]-input_length)/2):round((data.shape[0]+input_length)/2)]
+    else:
+        data = np.pad(data, (0, max(0, input_length - len(data))), "constant", constant_values=0)
+
+    return data
+
 def plot_confusion_matrix(preds, y_true, classes,
                           normalize=False,
                           title='Confusion matrix',
-                          cmap=plt.cm.Blues):
+                          cmap=plt.cm.Blues,
+                          plot = True):
     """
     This function prints and plots the confusion matrix.
     Normalization can be applied by setting `normalize=True`.
@@ -42,24 +123,25 @@ def plot_confusion_matrix(preds, y_true, classes,
         print('Confusion matrix, without normalization')
 
     #print(cm)
+    if plot:
+        plt.imshow(cm, interpolation='nearest', cmap=cmap)
+        plt.title(title)
+        plt.colorbar()
+        tick_marks = np.arange(len(classes))
+        plt.xticks(tick_marks, classes, rotation=45)
+        plt.yticks(tick_marks, classes)
 
-    plt.imshow(cm, interpolation='nearest', cmap=cmap)
-    plt.title(title)
-    plt.colorbar()
-    tick_marks = np.arange(len(classes))
-    plt.xticks(tick_marks, classes, rotation=45)
-    plt.yticks(tick_marks, classes)
+        fmt = '.2f' if normalize else 'd'
+        thresh = cm.max() / 2.
+        for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+            plt.text(j, i, format(cm[i, j], fmt),
+                     horizontalalignment="center",
+                     color="white" if cm[i, j] > thresh else "black")
 
-    fmt = '.2f' if normalize else 'd'
-    thresh = cm.max() / 2.
-    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-        plt.text(j, i, format(cm[i, j], fmt),
-                 horizontalalignment="center",
-                 color="white" if cm[i, j] > thresh else "black")
-
-    plt.tight_layout()
-    plt.ylabel('True label')
-    plt.xlabel('Predicted label')
+        plt.tight_layout()
+        plt.ylabel('True label')
+        plt.xlabel('Predicted label')
+    return cm
     
 def findScaler(x, scalerType='standard'):
     #initialize the scaler    
@@ -133,9 +215,8 @@ def train_test_creator(data, used, unkwown, depth, with_unknown = True, scalerTy
     if scalerType == 'robust' or scalerType == 'standard' :
         scalers = findScaler(dataset['Train'], scalerType)
         #scale data
-        scale(dataset['Train'], scalers)
-        scale(dataset['Test'], scalers)
-        scale(dataset['Validation'], scalers)
+        for key in group in dataset:
+            scale(dataset[group], scalers)
 
     #save used data for hyperas use
     with open('variables/train_test_split.pkl', 'wb') as f:  
